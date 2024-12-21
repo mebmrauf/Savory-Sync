dbe4de07de04793291573b10326dd604e40ca328
B M Rauf <115145418+mebmrauf@users.noreply.github.com>
2024-12-21 23:51:34 +0600

    create roleMiddleware

---
+++ backend/middleware/roleMiddleware.js
@@ -0,0 +1,9 @@
+// roleMiddleware.js
+export default function roleMiddleware(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+        next();
+    };
+}
